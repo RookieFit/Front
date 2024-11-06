74ff6242664b935f7593be709e6c1ec79d42fa35
aeaesi <hmp328hmp@gmail.com>
2024-11-06 23:48:43 +0900

    [Feat] #4 페이지 완, 그래프 중

--- rookiefit/src/myPage/myPage.tsx
+++ rookiefit/src/myPage/myPage.tsx
@@ -7,45 +7,60 @@ export default function myPage() {
   return (
     <section className="mypage">
       <div className='mypage_1'>
-       <div className='profile_up_box'>
+        <div className='profile_up_box'>
           <div className='profile'>
            <div className='profile_image'> <img src=""></img> </div>  {/* 그림 크기 조절*/}
             <div className='profile_nickname'>닉네임자리</div>
            <div className='profile_message'><textarea>"메시지를 입력하세요"</textarea></div>
            <div className='profile_edit'> 프로필 편집</div>
          </div>
-       </div>
-       <div className='myinfo'>
-         <div className='myinfo_up_box'>
-           <span className='myinfo_click'>내 정보</span>
+        </div>
+        <div className='myinfo'>
+          <div className='myinfo_up_box'>
+            <span className='myinfo_click'>내 정보</span>
             <span className='myinfo_edit'> 수정하기</span>
           </div>
           <div className='myinfo_box'>
-           <div className='myinfo_box_nickname'>
-             <span className= 'nickname_box'> 닉네임</span>
-             <span className= 'nickname_line'>닉네임 </span>
-           </div>
-           <div className='myinfo_box_name'>
-             이름 상자 이름</div>
-           <div className='myinfo_box_age'>
-             나이 상자 나이</div>
-           <div className='myinfo_box_weight'>
-             모무게 상자 몸무게</div>
-           <div className='myinfo_box_hight'>
-             키 상자 키</div>
-           <div className='myinfo_box_muscle'>
-             근육량 상자 근육량</div>
-           <div className='myinfo_box_fat'>
-             체지방량 상자 체지방량</div>
-           <div className='myinfo_box_address'>
-             주소 상자 주소</div>
-           <div className='myinfo_box_gym_name'>
-             헹스장명 사자 헬스장명</div>
+            <div className='myinfo_box_info'>
+              <span className= 'name_box'> 닉네임</span>
+              <span className= 'name_line'>닉네임 </span>
+            </div>
+            <div className='myinfo_box_info'>
+              <span className= 'name_box'> 이름</span>
+              <span className= 'name_line'>이름</span>
+            </div>
+            <div className='myinfo_box_info'>
+              <span className= 'name_box'> 나이</span>
+              <span className= 'name_line'>나이</span>
+            </div>
+            <div className='myinfo_box_info'>
+              <span className= 'name_box'> 몸무게</span>
+              <span className= 'name_line'>몸무게</span>
+            </div>
+            <div className='myinfo_box_info'>
+              <span className= 'name_box'>키</span>
+              <span className= 'name_line'>키</span>
+            </div>
+            <div className='myinfo_box_info'>
+              <span className= 'name_box'> 근력량</span>
+              <span className= 'name_line'>근력량</span>
+            </div>
+            <div className='myinfo_box_info'>
+              <span className= 'name_box'> 체지방</span>
+              <span className= 'name_line'>나체지방</span>
+            </div>
+            <div className='myinfo_box_info'>
+              <span className= 'name_box'> 주소</span>
+              <span className= 'name_line'>주소</span>
+            </div>
+            <div className='myinfo_box_info'>
+              <span className= 'name_box'> 체육관 이름</span>
+              <span className= 'name_line'>체육관 이름</span>
+            </div>   
           </div>
         </div>
      </div>
-    
-    <div className="mypage">
+     <div className="mypage_2">
         <div className='graph_box'>
           체중 변화 그래프
         </div>
